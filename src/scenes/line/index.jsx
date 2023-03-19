a352f66a627423460ad614a7ddea4c252b7aa236
Bryson Palmer <brysonpalmer@gmail.com>
2023-03-19 11:10:48 -0700

    Adding line chart component to line page.

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,5 +1,17 @@
-const Line = () => {
-  return <div>Line Graph</div>
+import { Box } from '@mui/material'
+
+import LineChart from '../../components/LineChart'
+import Header from '../../components/Header'
+
+const Pie = () => {
+  return (
+    <Box m='20px'>
+      <Header title='Line Chart' subtitle='Simple Line Chart' />
+      <Box height='75vh'>
+        <LineChart />
+      </Box>
+    </Box>
+  )
 }
 
-export default Line
+export default Pie
