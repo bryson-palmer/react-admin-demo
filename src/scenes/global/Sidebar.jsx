947a828268b267dcae7acc48ee26337c39070c71
Bryson Palmer <brysonpalmer@gmail.com>
2023-03-19 11:59:36 -0700

    Changed hard coded name to mine.

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -119,7 +119,7 @@ const Sidebar = () => {
                   fontWeight='bold'
                   sx={{ m: '10px 0 0 0'}}
                 >
-                  Ed Roh
+                  Bryson Palmer
                 </Typography>
                 <Typography variant='h5' color={colors.greenAccent[500]}>
                   VP Fancy Admin
